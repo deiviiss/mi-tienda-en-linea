6260a417cd0ebc825262083820458292e1d70cc5
David Hilera <david.hilera@hotmail.com>
2024-09-23 19:43:47 -0600

    refactor: Update background image behavior in HeaderHero component effect parallax

--- src/components/landing/HeaderHero.tsx
+++ src/components/landing/HeaderHero.tsx
@@ -4,7 +4,7 @@ import { Button } from '@/components/ui/button'
 export const HeaderHero = () => {
   return (
 
-    <section className='flex gap-2 relative sm:bg-[url("/imgs/banner.png")] bg-[url("/imgs/banner2.png")] bg-cover bg-top bg-no-repeat'>
+    <section className='flex gap-2 relative sm:bg-[url("/imgs/banner.png")] bg-[url("/imgs/banner2.png")] bg-fixed bg-cover bg-top bg-no-repeat'>
       <div className="container flex flex-col justify-center items-center text-center mx-auto h-screen max-w-[720px] px-1 z-10">
         <h1 className="text-5xl font-bold mb-6 tracking-wider pt-12">Transforma tu negocio con tu propia tienda en línea</h1>
         <p className="text-xl mb-8">Control total, sin comisiones ocultas, y soporte cercano en cada paso.</p>
