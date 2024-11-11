595b2a3279bcb65d74960bf7fb54b14cfa036765
David Hilera <david.hilera@hotmail.com>
2024-11-11 05:24:17 -0600

    style :lipstick: Update Tailwind plugin and add animations
    - Replaced `tailwindcss-animate` with `tailwindcss-animated` in `tailwind.config.ts` to improve animation options.
    - Applied `animate-fade-up` animation to header hero sections for smoother user experience.

--- src/components/landing/HeaderHero.tsx
+++ src/components/landing/HeaderHero.tsx
@@ -5,7 +5,7 @@ export const HeaderHero = () => {
   return (
 
     <section className='flex gap-2 relative sm:bg-[url("/imgs/banner.png")] bg-[url("/imgs/banner2.png")] bg-fixed bg-cover bg-top bg-no-repeat'>
-      <div className="container flex flex-col justify-center items-center text-center mx-auto h-screen max-w-[720px] px-1 z-10">
+      <div className="container flex flex-col justify-center items-center text-center mx-auto h-screen max-w-[720px] px-1 z-10 animate-fade-up">
         <h1 className="text-5xl font-bold mb-6 tracking-wider pt-12">Transforma tu negocio con tu propia tienda en línea</h1>
         <p className="text-xl mb-8">Control total, sin comisiones ocultas, y soporte cercano en cada paso.</p>
         <Button asChild size="lg" className="bg-blue-500 hover:bg-blue-600 text-white rounded-full">
