25ffbbc62e2dc209d2da3d3c772e46b47099dfee
David Hilera <david.hilera@hotmail.com>
2025-01-09 17:03:02 -0600

    chore: :label: Add meta tag for Envato site verification
    Added the `impact-site-verification` meta tag to validate the site with Envato. This ensures compliance with platform requirements for verification purposes.
    
    Facilitates Envato site validation for Mi Tienda en Línea.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,10 @@ export const metadata: Metadata = {
     default: 'Mi Tienda en Línea'
   },
   description:
-    'Crea tu tienda en línea con Mi Tienda en Línea. Vende tus productos en internet de forma fácil y segura.'
+    'Crea tu tienda en línea con Mi Tienda en Línea. Vende tus productos en internet de forma fácil y segura.',
+  other: {
+    'impact-site-verification': '7815ad31-0a0f-41eb-96b5-8a5ab4decb0e'
+  }
 }
 
 export default function RootLayout({
